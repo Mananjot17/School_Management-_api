320a99b0309ff5a930335ab2f8c63509a9aabec9
Manajot17 <mananjotsingh17@gmail.com>
2024-08-22 15:38:31 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv')
+const express = require('express');
+const mysql = require('mysql2');
+const schoolRoutes = require('./routes/schoolRoutes');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+    } else {
+        console.log('Connected to MySQL database');
+    }
+});
+
+app.use('/api/schools', schoolRoutes(db));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
